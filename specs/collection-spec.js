1585671020c70384c230281bbef1a661e4e6c2e9
Blak3Nick <blaken2021@gmail.com>
2016-11-02 18:37:05 -0400

    working with TDD

--- specs/collection-spec.js
+++ specs/collection-spec.js
@@ -1,5 +1,18 @@
 describe('Collection', function () {
-   it("should be true", function () {
-     expect(true).toBe(true);
+   var emptyCollection;
+    var oneCollection;
+    var manyCollection;
+
+    beforeEach(function () {
+        emptyCollection = new Collection();
+        oneCollection = new Collection();
+        manyCollection = new Collection();
+   });
+    describe("is empty", function () {
+     it("should return true", function () {
+
+         expect(emptyCollection.isEmpty()).toBe(true);
+     });
+
    });
 });
---
+++ specs/collection.js
@@ -0,0 +1,9 @@
+var Collection = function () {
+    var size;
+    this.isEmpty = function () {
+      return size === 0;
+  }
+  this.add = function () {
+      
+  }
+};
